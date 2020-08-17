66400d06a5b53dee8f7861b88a4c092aad5fd135
Amindi <amindiperera1@gmail.com>
2020-08-17 08:22:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aboutusui/Menu_Drawer.java
@@ -0,0 +1,14 @@
+package com.example.aboutusui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Drawer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__drawer);
+    }
+}
---
+++ app/src/main/java/com/example/aboutusui/Terms_Conditions.java
@@ -0,0 +1,14 @@
+package com.example.aboutusui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Terms_Conditions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms__conditions);
+    }
+}
